1f9f8a36cdca25c19342cb26cca9d7c38603f71a
unknown <arpithabekal04@gmail.com>
2025-06-05 03:53:40 +0530

    first commit

---
+++ Maventest5/src/main/java/Arpi12tha/App.java
@@ -0,0 +1,13 @@
+package Arpi12tha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
